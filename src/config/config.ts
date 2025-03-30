5fe658df420c98456d1fcf55ba5813ce96acf745
cerveux <cerveux@hotmail.com>
2025-03-30 04:52:49 -0300

    feat: authenticate user

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ dotenv.config();
    nameDatabase: string | undefined;
    userDatabase: string | undefined;
    passwordDatabase: string | undefined;
+   secret: string | undefined;
  }
 
 
@@ -18,4 +19,5 @@ export const config: InterfaceConfig = {
   nameDatabase: process.env.NAME_DATABASE,
   userDatabase: process.env.USER_DATABASE,
   passwordDatabase: process.env.PASSWORD_DATABASE,
+  secret: process.env.SECRET
 };
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,6 +1,8 @@
+import * as authValidator from "./auth.validators";
 import * as userValidator from "./user.validators";
 
 export {
+  authValidator,
   userValidator,
 };
 
